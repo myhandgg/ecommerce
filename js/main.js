c3ae561f5073eb30e53f687d791101876e09401a
eslam <eslamproo@yahoo.com>
2023-06-27 17:40:18 +0200

    finishied image slider

--- js/main.js
+++ js/main.js
@@ -15,4 +15,34 @@ function close() {
     overlay.style.display = "none"
 }
 
-// 
+// cart
+
+let cart = document.querySelector(".cart")
+let cartContainer = document.querySelector(".cart-container")
+
+
+cart.onclick = () => {
+    cartContainer.classList.toggle("show")
+}
+
+// show products
+
+let mainProduct = document.querySelector(".left-side .product")
+let popupImg = document.querySelector(".left-side-overlay")
+
+let leftArrow = document.querySelector(".prev")
+let rightArrow = document.querySelector(".next")
+
+let xMark = document.querySelector(".xmark")
+
+mainProduct.onclick = () =>{
+    popupImg.classList.add("show")
+    overlay.classList.add("show")
+    leftArrow.classList.add("show")
+    rightArrow.classList.add("show")
+}
+
+xMark.onclick = () => {
+    popupImg.classList.remove("show")
+    overlay.classList.remove("show")
+}
