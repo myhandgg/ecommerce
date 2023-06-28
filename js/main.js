e17ff09d9b7ac85e6d94d123caf7a315baec4b18
eslam <eslamproo@yahoo.com>
2023-06-28 15:35:54 +0200

    finished the project

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // bars
+
 let ul = document.querySelector("header ul")
 let overlay = document.querySelector(".overlay")
 
@@ -19,7 +20,6 @@ function close() {
 
 let cart = document.querySelector(".cart")
 let cartContainer = document.querySelector(".cart-container")
-
 
 cart.onclick = () => {
     cartContainer.classList.toggle("show")
@@ -45,4 +45,137 @@ mainProduct.onclick = () =>{
 xMark.onclick = () => {
     popupImg.classList.remove("show")
     overlay.classList.remove("show")
+}
+
+// moving imgaes
+
+let changeImg = document.querySelector(".left-side-overlay .product")
+
+let imgOne = "images/image-product-1.jpg"
+let imgTwo = "images/image-product-2.jpg"
+let imgThree = "images/image-product-3.jpg"
+let imgFour = "images/image-product-4.jpg"
+
+let images = [imgOne , imgTwo , imgThree , imgFour]
+
+let count = 0
+
+let thumbnail = document.querySelectorAll("#popup-thumbnails img")
+let mainThumbnail = document.querySelectorAll("#main-thumbnails img")
+
+let productOne = document.querySelector("#main-thumbnails .product-one")
+let productTwo = document.querySelector("#main-thumbnails .product-two")
+let productThree = document.querySelector("#main-thumbnails .product-three")
+let productFour = document.querySelector("#main-thumbnails .product-four")
+
+productOne.onclick = () => {
+    mainThumbnail.forEach((e) =>{
+        e.classList.remove("active")
+    })
+    productOne.classList.add("active")
+    mainProduct.src = imgOne
+}
+
+productTwo.onclick = () => {
+    mainThumbnail.forEach((e) =>{
+        e.classList.remove("active")
+    })
+    productTwo.classList.add("active")
+    mainProduct.src = imgTwo
+}
+
+productThree.onclick = () => {
+    mainThumbnail.forEach((e) =>{
+        e.classList.remove("active")
+    })
+    productThree.classList.add("active")
+    mainProduct.src = imgThree
+}
+
+productFour.onclick = () => {
+    mainThumbnail.forEach((e) =>{
+        e.classList.remove("active")
+    })
+    productFour.classList.add("active")
+    mainProduct.src = imgFour
+}
+
+rightArrow.onclick = moveRight
+leftArrow.onclick = moveLeft
+
+function moveRight() {
+    count++
+    if (count === images.length) {
+        count = 0
+    }
+    changeImg.src = images[count]
+    thumbnail.forEach((e) =>{
+        e.classList.remove("active")
+    })
+    thumbnail[count].classList.add("active")
+    mainThumbnail.forEach((e) =>{
+        e.classList.remove("active")
+    })
+    mainThumbnail[count].classList.add("active")
+    mainProduct.src = images[count]
+}
+
+function moveLeft() {
+    count--
+    if (count < 0) {
+        count = images.length - 1
+    }
+    changeImg.src = images[count]
+    thumbnail.forEach((e) =>{
+        e.classList.remove("active")
+    })
+    thumbnail[count].classList.add("active")
+    mainThumbnail.forEach((e) =>{
+        e.classList.remove("active")
+    })
+    mainThumbnail[count].classList.add("active")
+    mainProduct.src = images[count]
+}
+
+function mobileMoveRight() {
+    count++
+    if (count === images.length) {
+        count = 0
+    }
+    mainProduct.src = images[count]
+    thumbnail.forEach((e) =>{
+        e.classList.remove("active")
+    })
+    thumbnail[count].classList.add("active")
+    mainProduct.src = images[count]
+}
+
+function mobileMoveLeft() {
+    count--
+    if (count < 0) {
+        count = images.length - 1
+    }
+    mainProduct.src = images[count]
+    thumbnail.forEach((e) =>{
+        e.classList.remove("active")
+    })
+    thumbnail[count].classList.add("active")
+    mainProduct.src = images[count]
+}
+
+// buying section
+
+let minus = document.querySelector(".minus")
+let amount = document.querySelector(".number-of-products p")
+let plus = document.querySelector(".plus")
+
+minus.onclick = function() {
+    amount.innerHTML = amount.innerHTML - 1
+    if (+amount.innerHTML <= 0) {
+        amount.innerHTML = 0
+    }
+}
+
+plus.onclick = function() {
+    amount.innerHTML = +amount.innerHTML + 1
 }
